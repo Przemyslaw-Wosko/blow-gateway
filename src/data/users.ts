5331882ac849d6dbf9b0f26897d23cb5eab73134
Maciej Chmielarski <m.chmielarski@gmail.com>
2016-02-18 11:11:24 +0100

    add sample validator

--- src/data/users.ts
+++ src/data/users.ts
@@ -1,5 +1,6 @@
 import {Observable} from 'rxjs';
-import {Collection} from 'blow-server';
+import {Collection, Query} from 'blow-server';
+import {Validator, ValidationResult} from 'blow-validate';
 import {getCollection} from '../server';
 
 export class UserRaw {
@@ -10,10 +11,35 @@ export class UserRaw {
 
 const collection: Collection<UserRaw> = getCollection<UserRaw>('users');
 
+const newUserValidator = Validator.create({
+  email: {
+    string: true,
+    email: true,
+    uniqueEmail: function(property) {
+      const value = this[property];
+      const query = Query.create().equal('email', value);
+      return collection.count(query)
+        .filter(count => count > 1)
+        .mapTo({
+          path: property,
+          type: 'uniqueEmail',
+          message: `Email "${value}" is already in use.`
+        });
+    }
+  }
+});
+
 export class UserModel {
 
-  static create(data: UserRaw): Observable<UserRaw> {
-    return collection.save(data);
+  static create(data: UserRaw): Observable<UserRaw | ValidationResult> {
+    return newUserValidator.validate(data)
+      .mergeMap(result => {
+        if (result.isValid) {
+          return collection.save(data);
+        } else {
+          return Observable.of(result);
+        }
+      });
   }
 
   static find(): Observable<UserRaw> {
